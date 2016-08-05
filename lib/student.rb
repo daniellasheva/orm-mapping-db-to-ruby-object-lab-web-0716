a3a780f2a4f360deba48ced5502d78d8e3e143e9
daniellasheva <daniellasheva@gmail.com>
2016-08-05 16:21:48 -0400

    initial commit

--- lib/student.rb
+++ lib/student.rb
@@ -1,20 +1,84 @@
 class Student
   attr_accessor :id, :name, :grade
 
-  def self.new_from_db(row)
-    # create a new Student object given a row from the database
+  def self.new_from_db(row) ## we already have the info from the database
+    student= Student.new
+    student.id =row[0]
+    student.name= row[1]
+    student.grade= row[2]
+    student
   end
 
   def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+      end
+
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
 
   def self.find_by_name(name)
+    sql= <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+    SQL
+    student= DB[:conn].execute(sql, name).first  #RETURNS ARRAY
+    new_from_db(student)
     # find the student in the database given a name
     # return a new instance of the Student class
   end
+
+  def self.count_all_students_in_grade_9
+    sql= <<-SQL
+      SELECT COUNT(*) FROM students
+      WHERE grade = 9
+    SQL
+      DB[:conn].execute(sql)
+  end
   
+  def self.students_below_12th_grade
+     sql= <<-SQL
+      SELECT * FROM students
+      WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.first_x_students_in_grade_10 (variable)
+     sql= <<-SQL
+      SELECT * FROM students
+      WHERE grade =10
+      LIMIT ?
+    SQL
+      DB[:conn].execute(sql, variable)
+  end
+
+  def self.first_student_in_grade_10
+      sql= <<-SQL
+      SELECT * FROM students
+      WHERE grade =10
+      LIMIT 1
+    SQL
+      student=DB[:conn].execute(sql).first
+      new_from_db(student)
+  end
+
+  def self.all_students_in_grade_X (variable)
+     sql= <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+    SQL
+      DB[:conn].execute(sql, variable).map do |row|
+        new_from_db(row)
+      end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
